33316f2722c6898ecd30b3e0fb7b10e2bc259fc7
Oksana <oksanaparakonnaa32@gmail.com>
2023-09-16 17:27:18 +0300

    last commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,10 @@
-const list = document.querySelector(".js-list");
-console.dir(list);
-console.log([...list.children]);
-console.dir(list.textContent);
+function addListCategoryElem() {
+    const itemsCategory = document.querySelectorAll('li.item');
+    itemsCategory.forEach(item => {
+        return console.log(`Category: ${item.firstElementChild.textContent}
+        Elements: ${item.lastElementChild.children.length}
+        Numbers of Category: ${item.firstElementChild.children.length}`);
+    });
+
+}
+addListCategoryElem();
