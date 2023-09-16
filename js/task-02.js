c5022ac1b35cb8982c3e3cd20e87ab0e75376ad4
Oksana <oksanaparakonnaa32@gmail.com>
2023-09-16 21:11:48 +0300

    add li in to list

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,12 @@ const ingredients = [
   "Condiments",
 ];
 
-// same text
+const domElement = document.getElementById("ingredients");
+
+ingredients.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item;
+  li.classList.add("item");
+
+  domElement.appendChild(li);
+});
